a89baf7a562e127a601352f45ac6f6669c0331c0
Joseph211219479 <jd.josephdavids@gmail.com>
2015-08-01 19:00:31 +0200

    first class added

--- src/main/java/demo/domain/Person.java
+++ src/main/java/demo/domain/Person.java
@@ -1,7 +1,18 @@
 package demo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.*;
 /**
- * Created by root on 2015/08/01.
+ * Created by Root on 2015/08/01.
  */
+@Document
 public class Person {
+    @Id
+    private String id;
+
+
+
 }
